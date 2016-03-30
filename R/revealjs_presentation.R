8d845bb33d92c47e91149f6510c8beed0c104eef
Michael Lerch <mdlerch@gmail.com>
2016-03-29 19:50:10 -0600

    Interpret slide_level in YAML

--- R/revealjs_presentation.R
+++ R/revealjs_presentation.R
@@ -108,7 +108,8 @@ revealjs_presentation <- function(incremental = FALSE,
   args <- c(args, pandoc_variable_arg("center", jsbool(center)))
   
   # slide level
-  args <- c(args, "--slide-level", "2")
+  if (!is.null(slide_level))
+    args <- c(args, "--slide-level", as.character(slide_level))
   
   # theme
   theme <- match.arg(theme, revealjs_themes())
