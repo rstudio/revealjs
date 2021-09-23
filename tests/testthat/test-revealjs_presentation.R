ca1b7401f2704190e92dd59ff13deea7101478ca
christophe dervieux <christophe.dervieux@gmail.com>
2021-09-23 14:57:27 +0200

    test toc-title too

--- tests/testthat/test-revealjs_presentation.R
+++ tests/testthat/test-revealjs_presentation.R
@@ -3,7 +3,7 @@ test_that("toc argument works", {
   skip_if_not_installed("xml2")
   rmd <- local_temp_draft()
   html <- .render_and_read(
-    rmd, 
+    rmd,
     output_options = list(
       toc = TRUE,
       pandoc_args = c(pandoc_variable_arg("toc-title", "TOC"))
@@ -11,6 +11,10 @@ test_that("toc argument works", {
   )
   toc <- xml2::xml_find_all(html, "//section[@id='TOC']")
   expect_length(toc, 1)
-  xml2::xml_find_all(toc, "./nav/*[contains(@id, 'toc-title')]")
-  expect_equal(xml2::xml_name(xml2::xml_child(toc)), "nav")
+  expect_equal(
+    xml2::xml_text(
+      xml2::xml_find_all(toc, "./nav/*[contains(@id, 'toc-title')]")
+    ),
+    "TOC"
+  )
 })
